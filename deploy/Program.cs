f26aaa51afc7d8ef68da4582a49ef18bd7c8285c
Mishra <dmishr15@ms.ds.uhc.com>
2018-12-17 16:27:12 +0530

    Add project files.

---
+++ deploy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deploy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Convert.ToInt16(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
